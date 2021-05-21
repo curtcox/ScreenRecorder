17ec3db217edd46042d1faae9ee4ad8dcd1a5eb1
Curt Cox <curtcox@gmail.com>
2021-05-20 20:06:10 -0500

    Refactor.

--- src/main/java/com/curtcox/app/RasterSerializer.java
+++ src/main/java/com/curtcox/app/RasterSerializer.java
@@ -10,12 +10,12 @@ final class RasterSerializer {
 
     private final Dimension dim;
     private final WritableRaster raster;
-    private final int numBands = 3;
+    private final int numBands = Consts.bands;
 
     RasterSerializer(BufferedImage image) {
         this.dim     = dim(image);
         raster       = image.getRaster();
-        if (raster.getNumBands() != 3) {
+        if (raster.getNumBands() != numBands) {
             throw new IllegalArgumentException("Sorry, Dave.");
         }
     }
@@ -25,16 +25,15 @@ final class RasterSerializer {
     }
     
     ByteBuffer getPixelBytes() {
-        final int[] dataElements = dataElements();
-        final int         length = Array.getLength(dataElements);
-        return writeToBuffer(dataElements,numBands,length);
+        return writeToBuffer(dataElements());
     }
 
     private int[] dataElements() {
         return (int[]) raster.getDataElements(0, 0, dim.width, dim.height, null);
     }
 
-    private ByteBuffer writeToBuffer(int[] dataElements, int numBands, int length) {
+    private ByteBuffer writeToBuffer(int[] dataElements) {
+        final int length = Array.getLength(dataElements);
         ByteBuffer out = ByteBuffer.allocate(length * numBands + 1);
         writeDataElements(dataElements,length,out);
         out.position(0);
