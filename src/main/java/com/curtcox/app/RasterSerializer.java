0edbcfe78aa215a8b791958b5dff29adb0fd1b38
Curt Cox <curtcox@gmail.com>
2021-05-20 21:04:19 -0500

    Refactor.

--- src/main/java/com/curtcox/app/RasterSerializer.java
+++ src/main/java/com/curtcox/app/RasterSerializer.java
@@ -3,7 +3,6 @@ package com.curtcox.app;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.WritableRaster;
-import java.lang.reflect.Array;
 import java.nio.ByteBuffer;
 
 final class RasterSerializer {
@@ -33,19 +32,17 @@ final class RasterSerializer {
     }
 
     private ByteBuffer writeToBuffer(int[] dataElements) {
-        final int length = Array.getLength(dataElements);
-        ByteBuffer out = ByteBuffer.allocate(length * numBands + 1);
-        writeDataElements(dataElements,length,out);
+        ByteBuffer out = ByteBuffer.allocate(dataElements.length * numBands + 1);
+        writeDataElements(dataElements,out);
         out.position(0);
         out.limit(out.limit() - 1);
         return out;
     }
 
-    private void writeDataElements(int[] dataElements, int length,ByteBuffer out) {
+    private void writeDataElements(int[] dataElements,ByteBuffer out) {
         int index = 0;
-        for (int i = 0; i < length; i++) {
-            final int e = dataElements[i];
-            out.putInt(index, e << 8);
+        for (int i = 0; i < dataElements.length; i++) {
+            out.putInt(index, dataElements[i] << 8);
             index += 3;
         }
     }
