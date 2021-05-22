0e22f4d7b94c6e9df684cde12b408522e13b71c1
Curt Cox <curtcox@gmail.com>
2021-05-22 00:25:37 -0500

    After much frustration, copying code from Robot worked...

--- src/main/java/com/curtcox/app/Test.java
+++ src/main/java/com/curtcox/app/Test.java
@@ -2,20 +2,26 @@ package com.curtcox.app;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.awt.image.DataBuffer;
-import java.awt.image.WritableRaster;
+import java.awt.image.*;
 
 final class Test {
 
-    static BufferedImage image(int[] data, int width, int height) {
-        BufferedImage bufferedImage = new BufferedImage(width, height, DataBuffer.TYPE_INT);
-        WritableRaster raster = bufferedImage.getRaster();
-        raster.setDataElements(0,0,width,height,data);
-        print("raster = " + raster);
-        bufferedImage.setData(raster);
-        print("bufferedImage = " + bufferedImage);
-        return bufferedImage;
+    // After much frustration, copying code from Robot worked...
+    static BufferedImage image(int[] pixels, int width, int height) {
+        DataBufferInt buffer = new DataBufferInt(pixels, pixels.length);
+        DirectColorModel screenCapCM = new DirectColorModel(24,
+                /* red mask */ 0x00FF0000,
+                /* green mask */ 0x0000FF00,
+                /* blue mask */ 0x000000FF);
+        int[] bandmasks = new int[3];
+
+        bandmasks[0] = screenCapCM.getRedMask();
+        bandmasks[1] = screenCapCM.getGreenMask();
+        bandmasks[2] = screenCapCM.getBlueMask();
+
+        WritableRaster raster = Raster.createPackedRaster(buffer, width, height, width, bandmasks, null);
+
+        return new BufferedImage(screenCapCM, raster, false, null);
     }
 
     private static void show(String title, BufferedImage image) {
@@ -34,8 +40,10 @@ final class Test {
         print(Screen.width() + "*" + Screen.height() + " = " + Screen.width()*Screen.height());
         BufferedImage original = Screen.shot();
         show("original",original);
+
         RasterSerializer serializer = new RasterSerializer(original);
-        show("copy", image(serializer.data(),Screen.width(),Screen.height()));
+        BufferedImage copy = image(serializer.data(),Screen.width(),Screen.height());
+        show("copy", copy);
     }
 
     public static void main(String[] args) {
